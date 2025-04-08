cec33e5a27a12f790876eae291897fc9a78eb30b
Thomas <taraya2@illinois.edu>
2025-04-08 10:30:47 -0500

    version 1

---
+++ src/test/java/com/email/ai/AiEmailWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
